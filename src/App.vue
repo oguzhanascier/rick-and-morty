ae84c42d481d18bf580b2267bd88550cd9a83b12
oguzhanascier <oguzhanascier20@gmail.com>
2022-11-24 15:44:14 +0300

    api access

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button>MORTİ</button>
+    <div>
+      <ul v-for="i in item" :key="i.id">
+        <li>
+          {{ i.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      num: [],
+      item: [
+        { id: "", name: "", status: "", species: "", type: "", location: "" },
+      ],
+    };
+  },
+  created() {
+    for (let i = 1; i < 800; i++) {
+      this.num.push(i);
+    }
+    this.num.forEach((i) => {
+      axios
+        .get(`https://rickandmortyapi.com/api/character/${i}`)
+        .then((res) => {
+          let rick = res.data;
+        });
+    });
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from "axios";
+// import axios from "axios";
+
 Vue.config.productionTip = false
 
 new Vue({
